2a7eb847ede730150b628e5146a545baba605a78
David Garay <garaekz@gmail.com>
2024-04-13 04:43:14 -0700

    Set base json response format

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,5 +4,30 @@ namespace App\Controllers;
 
 class Controller
 {
-    //
+    protected function jsonResponse($data, $status = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        echo json_encode([
+            'status' => $status,
+            'data' => $data
+        ]);
+        exit;
+    }
+
+    protected function jsonPaginate($data, $status = 200)
+    {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        echo json_encode([
+            'status' => $status,
+            'data' => $data['data'],
+            'meta' => [
+                'current_page' => $data['current_page'],
+                'per_page' => $data['per_page'],
+                'total' => $data['total'],
+            ]
+        ]);
+        exit;
+    }
 }
