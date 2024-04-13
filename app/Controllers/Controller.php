62d607950eb73710b215da73838d4aaf2fd6a332
David Garay <garaekz@gmail.com>
2024-04-13 05:03:39 -0700

    Base controller fully doc

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,8 +2,20 @@
 
 namespace App\Controllers;
 
+/**
+ * The Controller class is a base class for all controllers in the application.
+ * It provides common methods for returning JSON responses.
+ */
 class Controller
 {
+    /**
+     * Returns a JSON response with the given data and status code.
+     *
+     * @param mixed $data The data to be included in the response.
+     * @param int $status The HTTP status code of the response. Default is 200.
+     * 
+     * @return void
+     */
     protected function jsonResponse($data, $status = 200)
     {
         header('Content-Type: application/json');
@@ -15,6 +27,14 @@ class Controller
         exit;
     }
 
+    /**
+     * Returns a paginated JSON response with the given data and status code.
+     *
+     * @param array $data The paginated data to be included in the response.
+     * @param int $status The HTTP status code of the response. Default is 200.
+     * 
+     * @return void
+     */
     protected function jsonPaginate($data, $status = 200)
     {
         header('Content-Type: application/json');
