29e0fdcdb51a5cccd4b2ad5d88198f50312bdd38
David Garay <garaekz@gmail.com>
2024-04-13 18:58:43 -0700

    Add a general response for helping in tests

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Garaekz\Services\HttpResponse;
+
 /**
  * The Controller class is a base class for all controllers in the application.
  * It provides common methods for returning JSON responses.
@@ -14,17 +16,22 @@ class Controller
      * @param mixed $data The data to be included in the response.
      * @param int $status The HTTP status code of the response. Default is 200.
      * 
-     * @return void
+     * @return void|HttpResponse
      */
     protected function jsonResponse($data, $status = 200)
     {
-        header('Content-Type: application/json');
-        http_response_code($status);
-        echo json_encode([
-            'status' => $status,
-            'data' => $data
-        ]);
-        exit;
+        $response = new HttpResponse(
+            json_encode(['status' => $status, 'data' => $data]),
+            $status,
+            ['Content-Type' => 'application/json']
+        );
+
+        if (env('APP_ENV') === 'testing') {
+            return $response;
+        } else {
+            $response->send();
+            exit;
+        }
     }
 
     /**
@@ -37,17 +44,13 @@ class Controller
      */
     protected function jsonPaginate($data, $status = 200)
     {
-        header('Content-Type: application/json');
-        http_response_code($status);
-        echo json_encode([
-            'status' => $status,
-            'data' => $data['data'],
+        return $this->jsonResponse([
+            'items' => $data['data'],
             'meta' => [
                 'current_page' => $data['current_page'],
                 'per_page' => $data['per_page'],
                 'total' => $data['total'],
             ]
-        ]);
-        exit;
+        ], $status);
     }
 }
